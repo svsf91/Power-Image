81cd2c97d9dd2a48fc7a7ca96b9bdd1decfb19a8
Hugh <deng.ch@husky.neu.edu>
2018-03-29 12:49:05 -0700

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { Routing } from './app.routing';
+import { SupportComponent } from './components/support/support.component';
+import { HeaderComponent } from './components/header-footer/header/header.component';
+import { FooterComponent } from './components/header-footer/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    SupportComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 import {Routes, RouterModule} from '@angular/router';
-import { MainPageComponent } from './components/main-page/main-page.component';
+import {MainPageComponent} from './components/main-page/main-page.component';
+import {SupportComponent} from './components/support/support.component';
 
 const appRoutes: Routes = [
-    {path: '', component: MainPageComponent}
+  {path: '', component: MainPageComponent},
+  {path: 'support', component: SupportComponent}
 ];
 
 export const Routing = RouterModule.forRoot(appRoutes);
