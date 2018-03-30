6d9a1365d884a0f2cd1fd9b694e2f19e00697b56
Hugh <deng.ch@husky.neu.edu>
2018-03-29 17:24:52 -0700

    fix bugs

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
 
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var router = require('./server/router.js');
+
+app.use(bodyParser);
+app.use(cookieParser);
+app.use(passport);
+
 app.use(express.static(__dirname + '/dist'));
+app.use('/api', router);
 app.listen(process.env.PORT || 4600);
+
+module.exports = app;
